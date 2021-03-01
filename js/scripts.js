f8e23e8d1b1428a33a8fcd33028a71d357881b05
ThiraTheNerd <thiragithinji@gmail.com>
2021-03-01 16:14:21 +0300

    add the js functions to display inputted data

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
-// contact constructor
+// contact constructor. Business Logic
 
 function Contact(first, last){
   this.FirstName = first;
   this.LastName = last;
 }
+
+// code to collect user input from the form . UI Logic
+
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'> " +newContact.FirstName+" </span></li>");
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
+
